10fda240f873574d6a2f1bfc0d451400ca25e765
Taimoor Arshad <taimoor.arshad43@gmail.com>
2025-05-08 13:59:02 -0400

    Adding refactors for Flask3 and other setUp() tearDown() changes

---
+++ test_message_model.py
@@ -0,0 +1,91 @@
+"""Message model tests."""
+
+# run these tests like:
+#
+#    python -m unittest test_message_model.py
+
+
+import os
+from unittest import TestCase
+
+from models import db, Message, User, Follows
+
+# BEFORE we import our app, let's set an environmental variable
+# to use a different database for tests (we need to do this
+# before we import our app, since that will have already
+# connected to the database
+
+os.environ['DATABASE_URL'] = "postgresql:///warbler-test"    # - Even with the refactor this still works and changes the database accordingly
+
+
+# Now we can import app
+
+from app import app, CURR_USER_KEY
+
+# Create our tables (we do this here, so we only create the tables
+# once for all tests --- in each test, we'll delete the data
+# and create fresh new clean test data
+
+# Will need to refactor some base code to use Flask 3
+
+# Creating new app instance with test database URI
+# app = create_app("postgressql:///warbler-test")
+
+# Configuring app in a similar way to how it would be in production
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = False
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
+
+# Disable some of Flasks error behavior and disabling debugtoolbar. Disabling CSRF token.
+# Don't have WTForms use CSRF at all, since it's a pain to test
+app.config['TESTING'] = True
+app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
+app.config['WTF_CSRF_ENABLED'] = False
+
+# with app.app_context():
+#     db.create_all()
+
+class MessageViewTestCase(TestCase):
+    """Test views for messages."""
+
+    def setUp(self):
+        """Create test client, add sample data."""
+        # Moving table creation here so we can have similar keys when testing
+        with app.app_context():
+            db.create_all()
+            User.query.delete()
+            Message.query.delete()
+
+            self.client = app.test_client()
+
+            self.testuser = User.signup(username="testuser",
+                                        email="test@test.com",
+                                        password="testuser",
+                                        image_url=None)
+
+            self.testfollowing = User.signup(username="testfollowing",
+                                        email="following@test.com",
+                                        password="testuser",
+                                        image_url=None)
+            
+            self.testfollower = User.signup(username="testfollower",
+                                        email="follower@test.com",
+                                        password="testuser",
+                                        image_url=None)
+
+            # Setting up diifferent followers and following for test user
+            self.testuser.following.append(self.testfollower)
+            self.testfollower.following.append(self.testuser)
+
+            db.session.commit()
+    
+    def tearDown(self):
+
+        """
+        Rolling back database, dropping all tables
+        """
+        # Will drop tables here and recreate them in setUp()
+        with app.app_context():
+            db.drop_all()
+            db.session.rollback()
--- test_user_model.py
+++ test_user_model.py
@@ -8,29 +8,28 @@
 import os
 from unittest import TestCase
 
-from models import db, User, Message, Follows
+from models import db, Message, User, Follows
 
 # BEFORE we import our app, let's set an environmental variable
 # to use a different database for tests (we need to do this
 # before we import our app, since that will have already
 # connected to the database
 
-os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
+os.environ['DATABASE_URL'] = "postgresql:///warbler-test"    # - Even with the refactor this still works and changes the database accordingly
 
 
 # Now we can import app
 
-from app import create_app
+from app import app, CURR_USER_KEY
 
 # Create our tables (we do this here, so we only create the tables
 # once for all tests --- in each test, we'll delete the data
 # and create fresh new clean test data
 
-
 # Will need to refactor some base code to use Flask 3
 
 # Creating new app instance with test database URI
-app = create_app("postgressql:///warbler-test")
+# app = create_app("postgressql:///warbler-test")
 
 # Configuring app in a similar way to how it would be in production
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -39,25 +38,60 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
 
 # Disable some of Flasks error behavior and disabling debugtoolbar. Disabling CSRF token.
+# Don't have WTForms use CSRF at all, since it's a pain to test
 app.config['TESTING'] = True
 app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
 app.config['WTF_CSRF_ENABLED'] = False
 
-with app.app_context():
-    db.create_all()
+# with app.app_context():
+#     db.create_all()
 
 
-class UserModelTestCase(TestCase):
+
+class MessageViewTestCase(TestCase):
     """Test views for messages."""
 
     def setUp(self):
         """Create test client, add sample data."""
+        # Moving table creation here so we can have similar keys when testing
+        with app.app_context():
+            db.create_all()
+            User.query.delete()
+            Message.query.delete()
 
-        User.query.delete()
-        Message.query.delete()
-        Follows.query.delete()
+            self.client = app.test_client()
 
-        self.client = app.test_client()
+            self.testuser = User.signup(username="testuser",
+                                        email="test@test.com",
+                                        password="testuser",
+                                        image_url=None)
+
+            self.testfollowing = User.signup(username="testfollowing",
+                                        email="following@test.com",
+                                        password="testuser",
+                                        image_url=None)
+            
+            self.testfollower = User.signup(username="testfollower",
+                                        email="follower@test.com",
+                                        password="testuser",
+                                        image_url=None)
+
+            # Setting up diifferent followers and following for test user
+            self.testuser.following.append(self.testfollower)
+            self.testfollower.following.append(self.testuser)
+
+            db.session.commit()
+    
+    def tearDown(self):
+
+        """
+        Rolling back database, dropping all tables
+        """
+        # Will drop tables here and recreate them in setUp()
+        with app.app_context():
+            db.drop_all()
+            db.session.rollback()
+
 
     def test_user_model(self):
         """Does basic model work?"""
